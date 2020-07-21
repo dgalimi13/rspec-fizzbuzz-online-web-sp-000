b430d21856907a34b501d97d2cb310973d998d46
danny galimi <dannygalimi13@aim.com>
2020-07-21 19:21:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  if fizzbuzz == 3
-  
+def fizzbuzz(int)
+  if int % 3 == 0
+puts "Fizz"  
 end
